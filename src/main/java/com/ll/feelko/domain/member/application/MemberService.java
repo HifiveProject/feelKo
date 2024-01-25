8b1073413d671ab444bd4615308e9f70514b6309
pth2134 <xogns2134@gmail.com>
2024-01-25 11:48:28 +0900

    NotProd

--- src/main/java/com/ll/feelko/domain/member/application/MemberService.java
+++ src/main/java/com/ll/feelko/domain/member/application/MemberService.java
@@ -8,6 +8,10 @@ import com.ll.feelko.domain.member.entity.Member;
 import java.util.Optional;
 
 public interface MemberService {
-    public Member register(MemberRegisterDto registerDto);
+    Member register(MemberRegisterDto registerDto);
     Optional<Member> findByEmail(String email);
+
+    boolean emailIsExist(String email);
+
+    long getMemberCount();
 }
