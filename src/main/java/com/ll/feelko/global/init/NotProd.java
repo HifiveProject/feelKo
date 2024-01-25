b3e291e602734ac468d8d9b3c9acf7d7e40ce1ba
taehoon <109726278+pth2134@users.noreply.github.com>
2024-01-25 13:12:33 +0900

    global/init/NotProd 클래스 생성 (#4)
    * birthday nullable
    
    * NotProd

---
+++ src/main/java/com/ll/feelko/global/init/NotProd.java
@@ -0,0 +1,49 @@
+package com.ll.feelko.global.init;
+
+import com.ll.feelko.domain.member.application.MemberService;
+import com.ll.feelko.domain.member.dao.MemberRepository;
+import com.ll.feelko.domain.member.dto.MemberRegisterDto;
+import com.ll.feelko.domain.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("!prod")
+@Slf4j
+@RequiredArgsConstructor
+public class NotProd {
+    private final MemberService memberService;
+    private final MemberRepository memberRepository;
+
+    @Bean
+    public ApplicationRunner initNotProd() {
+        return args -> {
+            //테스트용 멤버 생성
+            if (memberService.getMemberCount() == 0) {
+                //admin생성  role때문에 그냥 엔티티로 만듬
+                Member admin = Member.builder()
+                        .email("admin")
+                        .password("admin")
+                        .roles("admin")
+                        .build();
+                memberRepository.save(admin);
+                //테스트 멤버 생성 일단 당장 속도때문에 스트림은 사용하지 않았음
+                MemberRegisterDto memberRegisterDto = new MemberRegisterDto(
+                        "test", "test", "test", null, "010-1111-1111", null);
+
+                for (int i = 1; i < 5; i++) {
+                    memberRegisterDto.setEmail("test"+i);
+                    memberService.register(memberRegisterDto);
+                }
+            }
+
+            //여기부터 추가하시면 됩니다.
+
+
+        };
+    }
+}
