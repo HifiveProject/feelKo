649f70d094cf7bf15149bbf25f717a1fbf3f7670
jyh1108 <kobcom12@naver.com>
2024-01-26 16:56:25 +0900

    Thymeleaf layout

--- src/main/java/com/ll/feelko/domain/main/api/MainController.java
+++ src/main/java/com/ll/feelko/domain/main/api/MainController.java
@@ -1,12 +1,16 @@
 package com.ll.feelko.domain.main.api;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class MainController {
     @GetMapping("/")
     public String showMain(){
+        log.info("called");
         return "/domain/main/mainpage";
     }
 
