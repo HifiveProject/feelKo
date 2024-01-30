268f405b616cbf501c8ed86616d4abef354da4d2
Joong Seok Hwang <hjs7949@naver.com>
2024-01-30 14:25:02 +0900

    BaseEntity Class

---
+++ src/main/java/com/ll/feelko/global/common/entity/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.ll.feelko.global.common.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@ToString(callSuper = true)
+public class BaseEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @CreatedDate
+    @Column(columnDefinition = "DATETIME default CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+}
