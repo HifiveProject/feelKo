5f4cb217847584ef320d47c3b84869c601e247d0
jyong yong hwa <95514445+jyh1108@users.noreply.github.com>
2024-01-30 17:31:47 +0900

    Mainpage (#21)
    * layout html/header, footer
    
    * mainpage
    
    * main page
    
    * Thymeleaf layout
    
    * exprienceList.html
    
    * chore : login nev, button etc..
    
    * Cheol (#19)
    
    * 2nd weekly
    
    * chore : login nev, button etc..
    
    * fix
    
    * mainpage-html, css 수정
    
    * chore: css fix, 링크 구현
    
    * chore: css fix, 링크 구현2
    
    ---------
    
    Co-authored-by: J-Cheol <lunaris0206@naver.com>
    Co-authored-by: 전화철 <117714970+J-Cheol@users.noreply.github.com>

--- src/main/java/com/ll/feelko/domain/main/api/MainController.java
+++ src/main/java/com/ll/feelko/domain/main/api/MainController.java
@@ -1,14 +1,23 @@
 package com.ll.feelko.domain.main.api;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class MainController {
     @GetMapping("/")
     public String showMain(){
-        return "/domain/main/main";
+        log.info("called");
+        return "/domain/main/mainpage";
     }
 
+    @GetMapping("/experienceList")
+    public String showExperienceList(){
+        log.info("called");
+        return "/domain/main/experienceList";
+    }
 
 }
