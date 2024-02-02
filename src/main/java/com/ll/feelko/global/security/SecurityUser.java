2d7b666aca14088d5d0123b3a88b01cf98cc5a2b
pth2134 <xogns2134@gmail.com>
2024-02-02 11:55:00 +0900

    SecurityUser추가 변경

--- src/main/java/com/ll/feelko/global/security/SecurityUser.java
+++ src/main/java/com/ll/feelko/global/security/SecurityUser.java
@@ -21,9 +21,10 @@ public class SecurityUser extends User implements OAuth2User {
         this.profileImage = profileImage;
     }
 
-    public SecurityUser(long id, String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+    public SecurityUser(long id, String username, String password, String profileImage, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
         this.id = id;
+        this.profileImage = profileImage;
     }
 
     @Override
